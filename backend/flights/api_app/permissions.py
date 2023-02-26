c21721cd58b19e506c87fd716c67c634eb59c07f
zivattias <ziv.attias7@gmail.com>
2023-02-26 10:13:56 +0200

    added permissions & notes.txt

---
+++ backend/flights/api_app/permissions.py
@@ -0,0 +1,5 @@
+from rest_framework.permissions import BasePermission
+
+class IsStaff(BasePermission):
+    def has_permission(self, request, view):
+        return bool(request.user and request.user.is_staff)
